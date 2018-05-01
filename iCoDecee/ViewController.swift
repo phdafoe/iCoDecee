aa0ef6ad9258bdf7c4edaf52715d50470722a2b4
Andres Felipe Ocampo Eljaiesk <aocampoe@everis.com>
2018-05-01 11:51:43 +0200

    +[added new features] - IBOutlets and IBactions -

--- iCoDecee/ViewController.swift
+++ iCoDecee/ViewController.swift
@@ -9,16 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //MARK: - IBOutlets
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    //MARK: - IBActions
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+    }
+    
+    
 
+    //MARK: - Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
 
 
 }
