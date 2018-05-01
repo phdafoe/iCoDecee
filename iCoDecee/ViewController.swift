059227ff5e3d8bc9f718bd858c5f260165764e55
Andres Felipe Ocampo Eljaiesk <aocampoe@everis.com>
2018-05-01 12:10:38 +0200

    +[new features] - added logic in IBactions -

--- iCoDecee/ViewController.swift
+++ iCoDecee/ViewController.swift
@@ -10,13 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    //MARK: - Local Variables
+    var randomDiceIndex1 = 0
+    var randomDiceIndex2 = 0
+    
     //MARK: - IBOutlets
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
     //MARK: - IBActions
     @IBAction func rollButtonPressed(_ sender: UIButton) {
+        randomDiceIndex1 = Int(arc4random_uniform(6))
+        randomDiceIndex2 = Int(arc4random_uniform(6))
         
+        diceImageView1.image = UIImage(named: NSString(format: "dice%d", randomDiceIndex1) as String)
+        diceImageView2.image = UIImage(named: NSString(format: "dice%d", randomDiceIndex2) as String)
     }
     
     
@@ -24,6 +32,8 @@ class ViewController: UIViewController {
     //MARK: - Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        diceImageView1.image = UIImage(named: "dice1")
+        diceImageView2.image = UIImage(named: "dice1")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
